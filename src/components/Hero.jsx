33c81c248fc72b8fc35e6f314ac59babb1d46058
Maurya-Karan <karanmaurya81002@gmail.com>
2024-05-11 16:28:43 +0530

    😅

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,7 @@ const Hero = () => {
           '/>
         </motion.div>
 
-      <div className={`absolute sm:bottom-0 bottom-16 w-full  flex justify-between  items-center sm:px-20 px-8 `}>
+      <div className={`absolute sm:bottom-0 bottom-16 w-full  flex justify-between  items-center ${styles.paddingX} `}>
         <a href='#about'>
           <div className='w-[16px] h-[40px] rounded-3xl border-4  border-secondary flex justify-center items-start p-[2px] opacity-10'>
             <motion.div  animate = {{y:[0,24,0]}} transition = {{ duration:1.5, repeat:Infinity, repeatType:'loop' }}
@@ -49,7 +49,7 @@ const Hero = () => {
               {
                 contacts.map((contact,index) => (
                   <a key={index} href={contact.source_link}>
-                  <img src={contact.icon} className='w-5 h-5'></img></a>
+                  <img src={contact.icon} className='w-6 h-6'></img></a>
                 ))
               }
             </motion.div>
